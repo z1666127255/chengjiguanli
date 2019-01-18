586fe19ab2b9c767966d5183f1d9561c531f842d
曾英峰 <1666127255@qq.com>
2019-01-18 11:00:17 +0800

    删除成绩

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-struct Student{
+struct Student
+{
     int yuwen;
     int shuxue;
     int yingyu;
     char name[100];
 };
-struct Student arr [100];
+struct Student arr[100];
 int index = 0;
 
-int main(){
+int main()
+{
 
     printf("xueshengchengjiguanli\n");
 
-    while(1){
+    while (1)
+    {
         printf("1----tianjiaxueshnrgchengji\n");
         printf("2----shanchuxueshengchengji\n");
         printf("3----xianshisuoyou\n");
@@ -26,46 +29,63 @@ int main(){
         printf("Qxz\n");
 
         int code;
-        scanf("%d",&code);
+        scanf("%d", &code);
 
-        if(code == 1){
+        if (code == 1)
+        {
             printf("Qsrxsxm\n");
-            scanf("%s",arr[index].name);
+            scanf("%s", arr[index].name);
 
             printf("Qsrxsywcj\n");
-            scanf("%d",&(arr[index].yuwen));
+            scanf("%d", &(arr[index].yuwen));
 
             printf("Qsrxssxcj\n");
-            scanf("%d",&(arr[index].shuxue));
+            scanf("%d", &(arr[index].shuxue));
 
             printf("Qsrxsyycj\n");
-            scanf("%d",&(arr[index].yingyu));
+            scanf("%d", &(arr[index].yingyu));
             index++;
         }
-        if(code == 2){
-            
+        if (code == 2)
+        {
+            if (index > 0)
+            {
+                printf("Sccg\n");
+                index--;
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+
+            else
+            {
+                printf("Wxsxx,Scsb\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
         }
-        if(code == 3){
-            
+        if (code == 3)
+        {
         }
-        if(code == 4){
-            
+        if (code == 4)
+        {
         }
-        if(code == 5){
-            
+        if (code == 5)
+        {
         }
-        if(code == 6){
-            
+        if (code == 6)
+        {
         }
-        if(code == 7){
-            
+        if (code == 7)
+        {
         }
-        if(code == 8){
+        if (code == 8)
+        {
             printf("tuichu\n");
             break;
         }
     }
 
     return 0;
-
 }
