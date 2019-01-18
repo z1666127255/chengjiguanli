e0d3db27961f564921355438853d584b9bc15459
曾英峰 <1666127255@qq.com>
2019-01-18 10:15:46 +0800

    基本框架

--- main.c
+++ main.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 
+struct Student{
+    int yuwen;
+    int shuxue;
+    int yingyu;
+    char name[100];
+};
+
 int main(){
 
-    printf("hello world!");
+    printf("xueshengchengjiguanli\n");
+
+    while(1){
+        printf("1----tianjiaxueshnrgchengji\n");
+        printf("2----shanchuxueshengchengji\n");
+        printf("3----xianshisuoyou\n");
+        printf("4----yewu1,syoyouzongfenbujigedexuesheng(<180)\n");
+        printf("5----yewu2,meiyikedoubujigedexuesheng\n");
+        printf("6----yewu3,xianshizongfenzuigaodexuesheng\n");
+        printf("7----yewu4,xianshipingjunfenzuigao,qiemeiyoubujigedexuesheng\n");
+        printf("8----Tc\n");
+
+        printf("Qxz\n");
+
+        int code;
+        scanf("%d",&code);
+
+        if(code == 1){
+
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+            
+        }
+        if(code == 6){
+            
+        }
+        if(code == 7){
+            
+        }
+        if(code == 8){
+            printf("tuichu\n");
+            break;
+        }
+    }
 
     return 0;
 
